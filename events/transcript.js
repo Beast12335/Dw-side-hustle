b74fd353ef8ceeca0ffa2503cfea2783f428a81a
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:50:12 +0530

    Create transcript.js

---
+++ events/transcript.js
@@ -0,0 +1,24 @@
+const { MessageAttachment } = require('discord.js');
+const fs = require('fs');
+
+module.exports = async (channel) => {
+  try {
+    // Fetch all messages in the channel
+    const messages = await channel.messages.fetch({ limit: 100 });
+    const messageData = messages.map((msg) => `${msg.createdAt.toISOString()} | ${msg.author.tag} (${msg.author.id}): ${msg.content}`);
+
+    // Create a .doc file with the message data
+    const fileName = `${channel.name}-transcript.doc`;
+    fs.writeFileSync(fileName, messageData.join('\n'));
+
+    // Send the transcript file as an attachment in the channel
+    const transcriptAttachment = new MessageAttachment(fileName);
+    await channel.send({ files: [transcriptAttachment] });
+
+    // Delete the transcript file after sending
+    fs.unlinkSync(fileName);
+
+  } catch (error) {
+    console.error('Error generating and sending transcript:', error);
+  }
+};
