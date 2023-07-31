afdc3f5634b7b8b173580a2ec4d116d2a8d3ea3c
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:40:39 +0530

    Create closeButton.js

---
+++ events/closeButton.js
@@ -0,0 +1,29 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'close_button') return;
+
+    try {
+      // Check if the user has admin permissions
+      if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+        return await interaction.reply({ content: 'You do not have permission to close this ticket.', ephemeral: true });
+      }
+
+      // Send a confirmation message with a red button to confirm the ticket closing
+      const confirmEmbed = {
+        color: 'RED',
+        description: 'Confirm you wish to close the ticket.',
+      };
+
+      const confirmButton = { customId: 'confirm_close', label: 'Confirm', style: 'DANGER' };
+
+      await interaction.reply({ embeds: [confirmEmbed], components: [{ type: 'ACTION_ROW', components: [confirmButton] }] });
+
+    } catch (error) {
+      console.error('Error handling close button interaction:', error);
+    }
+  },
+};
