5291a34310debd8e44de79226b3ed406cbf74a93
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:38:00 +0530

    Create addUsersButton.js

---
+++ events/addUsersButton.js
@@ -0,0 +1,36 @@
+const { MessageActionRow, MessageButton, MessageCollector } = require('discord.js');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'add_users') return;
+
+    try {
+      // Your code to add users to the channel goes here
+      // You can use MessageCollector to wait for user input for 10 seconds
+
+      // Example:
+      await interaction.reply({ content: 'Please provide the user ID you want to add to the channel.', ephemeral: true });
+      const collector = new MessageCollector(interaction.channel, { time: 10000 }); // Collect messages for 10 seconds
+      collector.on('collect', async (message) => {
+        // Add the user with the provided ID to the channel
+        const userIdToAdd = message.content;
+        const userToAdd = await interaction.guild.members.fetch(userIdToAdd);
+        if (!userToAdd) return; // User not found
+
+        await interaction.channel.permissionOverwrites.create(userToAdd, { VIEW_CHANNEL: true });
+
+        // Send a success message
+        await interaction.channel.send(`User with ID ${userIdToAdd} has been added to the channel.`);
+      });
+
+      collector.on('end', () => {
+        // Collection time expired, handle as needed
+      });
+
+    } catch (error) {
+      console.error('Error handling add users button interaction:', error);
+    }
+  },
+};
