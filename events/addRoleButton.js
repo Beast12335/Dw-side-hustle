07e228fd64f177d98379cd8ef526b007a6fbd06f
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:39:35 +0530

    Create addRoleButton.js

---
+++ events/addRoleButton.js
@@ -0,0 +1,36 @@
+const { MessageActionRow, MessageButton, MessageCollector } = require('discord.js');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'add_roles') return;
+
+    try {
+      // Your code to add roles to the channel goes here
+      // You can use MessageCollector to wait for user input for 10 seconds
+
+      // Example:
+      await interaction.reply({ content: 'Please provide the role ID you want to add to the channel.', ephemeral: true });
+      const collector = new MessageCollector(interaction.channel, { time: 10000 }); // Collect messages for 10 seconds
+      collector.on('collect', async (message) => {
+        // Add the role with the provided ID to the channel
+        const roleIdToAdd = message.content;
+        const roleToAdd = interaction.guild.roles.cache.get(roleIdToAdd);
+        if (!roleToAdd) return; // Role not found
+
+        await interaction.channel.permissionOverwrites.create(roleToAdd, { VIEW_CHANNEL: true });
+
+        // Send a success message
+        await interaction.channel.send(`Role with ID ${roleIdToAdd} has been added to the channel.`);
+      });
+
+      collector.on('end', () => {
+        // Collection time expired, handle as needed
+      });
+
+    } catch (error) {
+      console.error('Error handling add roles button interaction:', error);
+    }
+  },
+};
