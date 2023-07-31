d33a4237f5936b941171eb34e93a5b7e6a50bca1
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:51:21 +0530

    Create confirmDeleteButton.js

---
+++ events/confirmDeleteButton.js
@@ -0,0 +1,32 @@
+const transcript = require('../transcript');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'confirm_delete') return;
+
+    try {
+      // Check if the user has admin permissions
+      if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+        return await interaction.reply({ content: 'You do not have permission to confirm deleting this ticket.', ephemeral: true });
+      }
+
+      // Generate and send the transcript
+      await transcript(interaction.channel);
+
+      // Delete the ticket channel
+      await interaction.channel.delete();
+
+      // Send a confirmation message for deleting the ticket
+      const transcriptChannelId = '393829383'; // Replace with the desired channel ID to send the transcript
+      const transcriptChannel = interaction.guild.channels.cache.get(transcriptChannelId);
+      if (transcriptChannel) {
+        await transcriptChannel.send('The ticket has been deleted.');
+      }
+
+    } catch (error) {
+      console.error('Error handling confirm delete button interaction:', error);
+    }
+  },
+};
