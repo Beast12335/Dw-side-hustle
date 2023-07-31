fc810ccc6f9c28a2545cfdc679d815c52f7134e7
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:22:51 +0530

    Create completed.js

---
+++ commands/completed.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Permissions } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('completed')
+    .setDescription('Move the channel to the specified category.')
+    .setDefaultPermission(false)
+    .addStringOption(option =>
+      option
+        .setName('category')
+        .setDescription('Enter the category to move the channel to.')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    try {
+      // Check if the user has admin permissions
+      if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+        return await interaction.reply({ content: 'You need admin permissions to use this command.', ephemeral: true });
+      }
+
+      const category = interaction.options.getString('category');
+
+      // Get the channel from which the command is used
+      const channel = interaction.channel;
+      
+      // Find the category in the guild by name
+      const categoryObj = interaction.guild.channels.cache.find(ch => ch.type === 'GUILD_CATEGORY' && ch.name === category);
+      
+      if (!categoryObj) {
+        return await interaction.reply({ content: `Category "${category}" not found.`, ephemeral: true });
+      }
+
+      // Move the channel to the specified category
+      await channel.setParent(categoryObj.id);
+
+      await interaction.reply({ content: `Channel moved to the category "${category}" successfully.`, ephemeral: true });
+
+    } catch (error) {
+      console.error('Error executing /completed command:', error);
+      await interaction.reply({ content: 'An error occurred while executing this command.', ephemeral: true });
+    }
+  },
+};
