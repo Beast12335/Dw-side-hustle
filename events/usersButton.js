5bea7da001e34d2749a35900bd7844ee78b3f67e
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:37:30 +0530

    Create usersButton.js

---
+++ events/usersButton.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'view_users') return;
+
+    try {
+      // Check if the user has the role with ID 38292739
+      const roleId = '38292739';
+      if (!interaction.member.roles.cache.has(roleId)) {
+        return await interaction.reply({ content: 'You do not have permission to manage users in this ticket.', ephemeral: true });
+      }
+
+      // Send an embed with two buttons to add or remove users
+      const userOptionsEmbed = {
+        color: '#00ff00',
+        description: 'Choose an option to manage users:',
+      };
+
+      const addUsersButton = { customId: 'add_users', label: 'Add Users', style: 'PRIMARY' };
+      const removeUsersButton = { customId: 'remove_users', label: 'Remove Users', style: 'DANGER' };
+
+      await interaction.reply({ embeds: [userOptionsEmbed], components: [{ type: 'ACTION_ROW', components: [addUsersButton, removeUsersButton] }] });
+
+    } catch (error) {
+      console.error('Error handling users button interaction:', error);
+    }
+  },
+};
