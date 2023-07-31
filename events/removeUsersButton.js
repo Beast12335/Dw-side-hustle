eb49575e86ab1377746784c79a95e0e5a3198601
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-07-31 10:38:34 +0530

    Create removeUsersButton.js

---
+++ events/removeUsersButton.js
@@ -0,0 +1,36 @@
+const { MessageActionRow, MessageButton, MessageCollector } = require('discord.js');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId !== 'remove_users') return;
+
+    try {
+      // Your code to remove users from the channel goes here
+      // You can use MessageCollector to wait for user input for 10 seconds
+
+      // Example:
+      await interaction.reply({ content: 'Please provide the user ID you want to remove from the channel.', ephemeral: true });
+      const collector = new MessageCollector(interaction.channel, { time: 10000 }); // Collect messages for 10 seconds
+      collector.on('collect', async (message) => {
+        // Remove the user with the provided ID from the channel
+        const userIdToRemove = message.content;
+        const userToRemove = await interaction.guild.members.fetch(userIdToRemove);
+        if (!userToRemove) return; // User not found
+
+        await interaction.channel.permissionOverwrites.get(userToRemove.id)?.delete();
+
+        // Send a success message
+        await interaction.channel.send(`User with ID ${userIdToRemove} has been removed from the channel.`);
+      });
+
+      collector.on('end', () => {
+        // Collection time expired, handle as needed
+      });
+
+    } catch (error) {
+      console.error('Error handling remove users button interaction:', error);
+    }
+  },
+};
