b26468330e7cd86eae0ad866820b4ec7cc9f585a
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-03 21:49:36 +0530

    Update ticketMenuInteraction.js

--- events/ticketMenuInteraction.js
+++ events/ticketMenuInteraction.js
@@ -1,4 +1,4 @@
-const { Client, MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { Client, EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('discord.js');
 
 module.exports = {
   name: 'interactionCreate',
@@ -21,36 +21,36 @@ module.exports = {
       await interaction.reply({ content: 'Ticket has been created!', ephemeral: true });
 
       // Create the action row for buttons
-      const actionRow = new MessageActionRow()
+      const actionRow = new ActionRowBuilder()
         .addComponents(
-          new MessageButton()
+          new ButtonBuilder()
             .setCustomId('claim_ticket')
             .setLabel('Claim')
-            .setStyle('PRIMARY'),
-          new MessageButton()
+            .setStyle('ButtonStyle.Primary'),
+          new ButtonBuilder()
             .setCustomId('view_users')
             .setLabel('Users')
-            .setStyle('PRIMARY'),
-          new MessageButton()
+            .setStyle('ButtonStyle.Primary'),
+          new ButtonBuilder()
             .setCustomId('assign_role')
             .setLabel('Role')
-            .setStyle('PRIMARY'),
-          new MessageButton()
+            .setStyle('ButtonStyle.Primary'),
+          new ButtonBuilder()
             .setCustomId('close_ticket')
             .setLabel('Close')
-            .setStyle('DANGER'),
-          new MessageButton()
+            .setStyle('ButtonStyle.Danger'),
+          new ButtonBuilder()
             .setCustomId('delete_ticket')
             .setLabel('Delete')
-            .setStyle('DANGER'),
-          new MessageButton()
+            .setStyle('ButtonStyle.Danger'),
+          new ButtonBuilder()
             .setCustomId('get_transcript')
             .setLabel('Transcript')
-            .setStyle('SECONDARY')
+            .setStyle('ButtonStyle.Secondary')
         );
 
       // Send the welcome embed with buttons in the ticket channel
-      const welcomeEmbed = new MessageEmbed()
+      const welcomeEmbed = new EmbedBuilder()
         .setTitle(`Welcome to your ${selectedType} Ticket!`)
         .setDescription('Please be patient and avoid pinging. Our support team will assist you shortly.')
         .setColor('#00ff00');
