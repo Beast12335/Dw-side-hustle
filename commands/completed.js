88af1b17093bd142adbb4b5ba15dd7cd452e7902
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-03 20:51:20 +0530

    Update completed.js

--- commands/completed.js
+++ commands/completed.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions } = require('discord.js');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -16,7 +16,7 @@ module.exports = {
   async execute(interaction) {
     try {
       // Check if the user has admin permissions
-      if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      if (!interaction.member.permissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
         return await interaction.reply({ content: 'You need admin permissions to use this command.', ephemeral: true });
       }
 
