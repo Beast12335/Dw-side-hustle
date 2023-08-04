6b35ea661ba707479ca8fa958a8f7ab5f07d179c
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-04 19:45:22 +0530

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -8,9 +8,10 @@ module.exports = {
   async execute(interaction) {
     /*await interaction.deferReply()
     try {
-      */await interaction.deferReply()
+      *//*await interaction.deferReply()
       await wait(5000)
-      await interaction.followUp('pong');
+      await interaction.followUp('pong');*/
+      await interaction.reply('pong');
     /*} catch (error) {
       console.error('Error replying with pong:', error);
       await interaction.followUp({ content: 'An error occurred while executing this command.', ephemeral: true });
