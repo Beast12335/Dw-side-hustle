88a8084694317e68868a278a9d9322e6633b8681
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-04 12:30:28 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
-const { Canvas } = require('canvas');
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const {Canvas} = require('canvas');
+const {Client, GatewayIntentBits, Collection} = require('discord.js');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
 const fs = require('fs');
 require('dotenv').config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 client.commands = new Collection();
 
 const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
@@ -14,7 +14,9 @@ const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 const registerCommands = async () => {
   try {
     const commands = [];
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    const commandFiles = fs
+      .readdirSync('./commands')
+      .filter((file) => file.endsWith('.js'));
 
     for (const file of commandFiles) {
       const command = require(`./commands/${file}`);
@@ -22,10 +24,13 @@ const registerCommands = async () => {
       client.commands.set(command.data.name, command);
     }
 
-    const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
-    await rest.put(Routes.applicationGuildCommands(client.user.id, '860512303233236992'), {
-      body: commands,
-    });
+    const rest = new REST({version: '9'}).setToken(BOT_TOKEN);
+    await rest.put(
+      Routes.applicationGuildCommands(client.user.id, '860512303233236992'),
+      {
+        body: commands,
+      }
+    );
 
     console.log('Slash commands registered!');
   } catch (error) {
@@ -34,7 +39,7 @@ const registerCommands = async () => {
 };
 
 const cron = require('node-cron');
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 
 //Fnction to run the checkVouchers script daily
 function runCheckVouchersScript() {
@@ -59,7 +64,7 @@ function runCheckVouchersScript() {
   } catch (error) {
     console.error('Error running checkVouchers script:', error);
   }
-  }
+}
 // Event handler when the bot is ready
 client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}.`);
@@ -69,28 +74,30 @@ client.once('ready', () => {
 // Increase the maximum listener limit for EventEmitter
 require('events').EventEmitter.defaultMaxListeners = 15; // Adjust the value as needed
 
-// Event handler for slash command interactions
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) return;
-
-  const commandName = interaction.commandName;
-  const command = client.commands.get(commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error('Error executing command:', error);
-    await interaction.reply({ content: 'An error occurred while executing this command.', ephemeral: true });
-  }
-});
-
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync('./events')
+  .filter((file) => file.endsWith('.js'));
 for (const file of eventFiles) {
   const eventHandler = require(`./events/${file}`);
   if (eventHandler.name === 'interactionCreate') {
-    client.on('interactionCreate', eventHandler.execute);
+    if (interaction.isChatInputCommand()) {
+      const commandName = interaction.commandName;
+      const command = client.commands.get(commandName);
+
+      if (!command) return;
+
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error('Error executing command:', error);
+        await interaction.reply({
+          content: 'An error occurred while executing this command.',
+          ephemeral: true,
+        });
+      }
+    } else {
+      client.on('interactionCreate', eventHandler.execute);
+    }
   } else {
     client.on(eventHandler.name, (...args) => eventHandler.execute(...args));
   }
