0332f4383f55add8184c50e055b31222baf86731
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-05 22:11:54 +0530

    Update usersButton.js

--- events/usersButton.js
+++ events/usersButton.js
@@ -6,7 +6,7 @@ module.exports = {
 
     try {
       // Check if the user has the role with ID 38292739
-      const roleId = '38292739';
+      const roleId = '860538277710790706';
       if (!interaction.member.roles.cache.has(roleId)) {
         return await interaction.reply({ content: 'You do not have permission to manage users in this ticket.', ephemeral: true });
       }
@@ -17,10 +17,10 @@ module.exports = {
         description: 'Choose an option to manage users:',
       };
 
-      const addUsersButton = { customId: 'add_users', label: 'Add Users', style: 'ButtonStyle.Secondary' };
-      const removeUsersButton = { customId: 'remove_users', label: 'Remove Users', style: 'ButtonStyle.Danger' };
+      const addUsersButton = { customId: 'add_users', label: 'Add Users', style: 'Sucess' };
+      const removeUsersButton = { customId: 'remove_users', label: 'Remove Users', style: 'Danger' };
 
-      await interaction.reply({ embeds: [userOptionsEmbed], components: [{ type: 'ACTION_ROW', components: [addUsersButton, removeUsersButton] }] });
+      await interaction.reply({ embeds: [userOptionsEmbed], components: [{ type: 1, components: [addUsersButton, removeUsersButton] }] });
 
     } catch (error) {
       console.error('Error handling users button interaction:', error);
