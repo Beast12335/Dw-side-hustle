c5ebd3d4d338ff5a590aae2e2b6a4f12b2836632
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-07 16:14:51 +0530

    Update claimButton.js

--- events/claimButton.js
+++ events/claimButton.js
@@ -6,8 +6,8 @@ module.exports = {
 
     try {
       // Check if the user has the role with ID 38292739
-      const roleId = '914051151169716245';
-      if (!interaction.member.roles.cache.has(roleId)) {
+      //const roleId = '914051151169716245';
+      if (!interaction.member.permissions.has('ADMINISTRATOR')) {
         return await interaction.reply({ content: 'You do not have permission to claim this ticket.', ephemeral: true });
       }
 
