4228439de5e50bcae467d5108fa3f375c70d92b0
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-07 13:52:09 +0530

    Update transcript.js

--- events/transcript.js
+++ events/transcript.js
@@ -1,22 +1,16 @@
 const { AttachmentBuilder } = require('discord.js');
 const fs = require('fs');
-
+const trans = require('discord-html-transcripts');
 module.exports = async (channel) => {
   try {
-    // Fetch all messages in the channel
-    const messages = await channel.messages.fetch({ limit: 100 });
-    const messageData = messages.map((msg) => `${msg.createdAt.toISOString()} | ${msg.author.tag} (${msg.author.id}): ${msg.content}`);
-
-    // Create a .doc file with the message data
-    const fileName = `${channel.name}-transcript.doc`;
-    fs.writeFileSync(fileName, messageData.join('\n'));
-
-    // Send the transcript file as an attachment in the channel
-    const transcriptAttachment = new AttachmentBuilder(fileName);
-    await channel.send({ files: [transcriptAttachment] });
-
-    // Delete the transcript file after sending
-    fs.unlinkSync(fileName);
+    const attachment = await trans.createTranscript(channel,{
+      filename: channel.name,
+      saveImages: true,
+      poweredBy: false
+    });
+    channel.send({
+      files:[attachment]
+    });
 
   } catch (error) {
     console.error('Error generating and sending transcript:', error);
