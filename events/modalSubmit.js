4324c722ef362a298c3d63689be536b6cb0a68f7
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-08 16:22:34 +0530

    Create modalSubmit.js

---
+++ events/modalSubmit.js
@@ -0,0 +1,31 @@
+const { ActionRowBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: 'interactionCreate',
+  async execute(interaction) {
+    if (!interaction.isModalSubmit()) return;
+    if (interaction.customId !== 'form') return;
+
+    try {
+      const q1 = interaction.fields.getTextInputValue('q1')
+      const q2 = interaction.fields.getTextInputValue('q2')
+      const q3 = interaction.fields.getTextInputValue('q3')
+      
+      const embed = new Embed()
+      .setTitle('Form Submission')
+      .setColor('#ff00f0')
+      .setDescription(`${interaction.user.username} says:
+      1. What do you want to order?
+      > ${q1}
+      2. Tell us more about your order
+      > ${q2}
+      3. Any inspiration or source for your order?
+      > ${q3}`);
+      
+      await interaction.reply({ embeds: [embed]});
+
+    } catch (error) {
+      console.error('Error handling delete button interaction:', error);
+    }
+  },
+};
