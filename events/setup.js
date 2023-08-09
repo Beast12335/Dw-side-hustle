41087653ed9e1763f9306db12276ddf7d2c3ce55
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-09 21:23:26 +0530

    Update setup.js

--- events/setup.js
+++ events/setup.js
@@ -42,10 +42,11 @@ module.exports = {
           deny: PermissionsBitField.Flags.ViewChannel},
                               {
                                 id: interaction.user.id,
-                                allow: PermissionsBitField.ViewChannel},
-                               /* {id:914051151169716245,
-                                allow: PermissionsBitField.ViewChannel
-                                }*/
+                                allow: PermissionsBitField.Flags.ViewChannel},
+                               {id:`914051151169716245`,
+                                allow: PermissionsBitField.Flags.ViewChannel,
+                                type:0
+                                }
                                 ]
       });
       //await ticketChannel.permissionOverwrites.create(interaction.guild.id,{ViewChannel:false});
