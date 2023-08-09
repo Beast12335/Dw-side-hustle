17772d57fa137e1a60e5d090e48d8d9dda741dbf
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-09 13:26:23 +0530

    Create messageCreate.js

---
+++ events/messageCreate.js
@@ -0,0 +1,18 @@
+const {mysql} = require('mysql2');
+
+module.exports = {
+  name: 'messageCreate',
+  async execute(message) {
+    const connection = await mysql.createConnection(process.env.DB_URL)
+    console.log('connected to mysql db')
+    try {
+      if(message.channelId = '860512303233236995'){
+        await connection.execute('update money set balance = balance +0.1 where user = ?',[message.author.id])
+        await message.react('👍🏻')
+        await connection.end()
+        }
+    } catch (error) {
+      console.error('Error handling message create:', error);
+    }
+  },
+};
