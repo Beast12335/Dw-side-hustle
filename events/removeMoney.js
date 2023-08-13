a3b67bfaac5f0eaeb0bafebd4b2a421b19768b40
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-13 15:41:01 +0530

    Update removeMoney.js

--- events/removeMoney.js
+++ events/removeMoney.js
@@ -28,7 +28,7 @@ module.exports = {
             return;
             }
       
-          const [updateResult] = await connection.execute('UPDATE money SET balance = balance - ? WHERE name = ?', [coinsToAdd, selectedUser]);
+          const [updateResult] = await connection.execute('insert into money values('000',?,`-${coinsToAdd}`,?)', [selectedUser, expired]);
       
           if (updateResult.affectedRows === 1) {
             await interaction.followUp(`Successfully removed ${coinsToAdd} coins from ${selectedUser}'s balance.`);
