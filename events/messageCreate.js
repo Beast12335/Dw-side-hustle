d4577c032185f7ba1b9f22b946e1f5e2c81b9f77
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2023-08-14 16:42:10 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -7,8 +7,7 @@ module.exports = {
     const connection = await mysql.createConnection(process.env.DB_URL)
     console.log('connected to mysql db')
     try {
-      if(message.channelId = '860512303233236995'){
-        await connection.execute('update money set balance = balance +0.1 where user = ?',[message.author.id])
+      if(message.channelId == '860512303233236995'){
         await message.react('👍🏻')
         await connection.end()
         }
