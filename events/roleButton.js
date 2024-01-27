ff17c75526f8a155e3c4a11e682768db7288a9a1
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-27 12:44:44 +0530

    Update roleButton.js

--- events/roleButton.js
+++ events/roleButton.js
@@ -3,12 +3,12 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'assign_role') return;
-
+    await interaction.deferReply();
     try {
       // Check if the user has the role with ID 38292739
       //const roleId = '860538277710790706';
       if (!interaction.member.permissions.has('ADMINISTRATOR')) {
-        return await interaction.reply({ content: 'You do not have permission to manage roles in this ticket.', ephemeral: true });
+        return await interaction.followUp({ content: 'You do not have permission to manage roles in this ticket.', ephemeral: true });
       }
 
       // Send an embed with two buttons to add or remove roles
@@ -20,10 +20,11 @@ module.exports = {
       const addRolesButton = { type:2,custom_id: 'add_roles', label: 'Add Roles', style: 3 };
       const removeRolesButton = { type:2,custom_id: 'remove_roles', label: 'Remove Roles', style: 4 };
 
-      await interaction.reply({ embeds: [roleOptionsEmbed], components: [{ type: 1, components: [addRolesButton, removeRolesButton] }] });
+      await interaction.followUp({ embeds: [roleOptionsEmbed], components: [{ type: 1, components: [addRolesButton, removeRolesButton] }] });
 
     } catch (error) {
       console.error('Error handling role button interaction:', error);
+      await interaction.followUp(error);
     }
   },
 };
