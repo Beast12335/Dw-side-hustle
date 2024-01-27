5056a8c96512d623db18122ace30fbf8b04a2ac5
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-27 12:36:40 +0530

    Update addUsersButton.js

--- events/addUsersButton.js
+++ events/addUsersButton.js
@@ -5,13 +5,13 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'add_users') return;
-
+    await interaction.deferReply()
     try {
       // Your code to add users to the channel goes here
       // You can use MessageCollector to wait for user input for 10 seconds
 
       // Example:
-      await interaction.reply({ content: 'Please provide the user ID you want to add to the channel.', ephemeral: false });
+      await interaction.followUp({ content: 'Please provide the user ID you want to add to the channel.', ephemeral: false });
       const collector = interaction.channel.createMessageCollector( { time: 60000 }); // Collect messages for 10 seconds
       collector.on('collect', async (message) => {
         // Add the user with the provided ID to the channel
@@ -34,6 +34,7 @@ module.exports = {
       });
 
     } catch (error) {
+      await interaction.followUp(error);
       console.error('Error handling add users button interaction:', error);
     }
   },
