7cf33aede5350d8efd9c8b09082f32eecc5a8dab
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-27 12:37:43 +0530

    Update claimButton.js

--- events/claimButton.js
+++ events/claimButton.js
@@ -3,12 +3,12 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'claim_ticket') return;
-
+    await interaction.deferReply();
     try {
       // Check if the user has the role with ID 38292739
       //const roleId = '914051151169716245';
       if (!interaction.member.permissions.has('ADMINISTRATOR')) {
-        return await interaction.reply({ content: 'You do not have permission to claim this ticket.', ephemeral: true });
+        return await interaction.followUp({ content: 'You do not have permission to claim this ticket.', ephemeral: true });
       }
 
       // Send an embed saying the ticket has been claimed by the user
@@ -17,10 +17,11 @@ module.exports = {
         description: `Ticket has been claimed by ${interaction.user.username}.`,
       };
 
-      await interaction.reply({ embeds: [claimEmbed], ephemeral: false });
+      await interaction.followUp({ embeds: [claimEmbed], ephemeral: false });
 
     } catch (error) {
       console.error('Error handling claim button interaction:', error);
+      await interaction.followUp(error);
     }
   },
 };
