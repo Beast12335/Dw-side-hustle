d407acbab402f2dd223630a948c5a79955319d13
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-27 12:50:52 +0530

    Update usersButton.js

--- events/usersButton.js
+++ events/usersButton.js
@@ -3,12 +3,12 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'view_users') return;
-
+    await interaction.deferReply();
     try {
       // Check if the user has the role with ID 38292739
       //const roleId = '860538277710790706';
       if (!interaction.member.permissions.has('ADMINISTRATOR')) {
-        return await interaction.reply({ content: 'You do not have permission to manage users in this ticket.', ephemeral: true });
+        return await interaction.followUp({ content: 'You do not have permission to manage users in this ticket.', ephemeral: true });
       }
 
       // Send an embed with two buttons to add or remove users
@@ -20,10 +20,11 @@ module.exports = {
       const addUsersButton = { type:2,custom_id: 'add_users', label: 'Add Users', style: 3 };
       const removeUsersButton = { type:2,custom_id: 'remove_users', label: 'Remove Users', style: 4 };
 
-      await interaction.reply({ embeds: [userOptionsEmbed], components: [{ type: 1, components: [addUsersButton, removeUsersButton] }] });
+      await interaction.followUp({ embeds: [userOptionsEmbed], components: [{ type: 1, components: [addUsersButton, removeUsersButton] }] });
 
     } catch (error) {
       console.error('Error handling users button interaction:', error);
+      await interaction.followUp(error);
     }
   },
 };
