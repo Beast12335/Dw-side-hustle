2d3d60a69dc7599c2698cb2d58085e9f73742d51
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-01-27 13:16:58 +0530

    Update use voucher.js

--- commands/use voucher.js
+++ commands/use voucher.js
@@ -52,7 +52,7 @@ module.exports = {
 
     } catch (error) {
       console.error('Error executing /usevoucher command:', error);
-      await interaction.reply({ content: 'An error occurred while executing this command.', ephemeral: true });
+      await interaction.followUp({ content: 'An error occurred while executing this command.', ephemeral: true });
     }
   },
 };
