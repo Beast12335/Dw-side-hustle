6c4e68c55a5836d8d81c2226567af911e8f2b778
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-02-05 23:01:42 +0530

    Create staff order.js

---
+++ commands/staff order.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField } = require('discord.js');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('staff-order')
+    .setDescription('Record an order by staff.')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Select a user')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    try {
+      // Check if the user invoking the command is an admin
+      if (!interaction.member.permissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
+        return interaction.followUp('You do not have the necessary permissions to use this command.');
+      }
+      
+      const connection = await mysql.createConnection(process.env.DB_URL);
+      // Retrieve the selected user ID
+      const selectedUser = interaction.options.getUser('user');
+
+      // Get the current month and year
+      const currentDate = new Date();
+      const currentMonthYear = currentDate.toLocaleString('default', { month: 'long', year: 'numeric' });
+
+      // Store the user ID and current month-year into the MySQL table
+      const query = `INSERT INTO staff (id, date) VALUES (${selectedUser.id}, '${currentMonthYear}')`;
+      connection.query(query, (error) => {
+        if (error) {
+          console.error(error);
+          return interaction.followUp('Error storing data in the database.');
+        }
+
+        interaction.followUp(`Order for ${selectedUser.username} (${selectedUser.id}) have been added for ${currentMonthYear}.`);
+      });
+    } catch (error) {
+      console.error(error);
+      await interaction.followUp('Error processing the command.');
+    }
+  },
+};
