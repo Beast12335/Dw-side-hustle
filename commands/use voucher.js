bc1a2eb9429399e6dbd87a8b09e29b94cdc5c7e5
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-04-13 17:04:09 +0530

    Update use voucher.js

--- commands/use voucher.js
+++ commands/use voucher.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder } = require('discord.js');
-const mysql = require('mysql2/promise');
+const vouchers = require('../db/vouchers.js');
 require('dotenv').config();
 
 module.exports = {
@@ -19,16 +19,9 @@ module.exports = {
     try {
      // await interaction.deferReply()
       const voucherCode = interaction.options.getString('code');
-
-      // Connect to the MySQL server
-      const connection = await mysql.createConnection(
-        process.env.DB_URL
-      );
-
-      console.log('Connected to MySQL server.');
-
       // Check if the voucher code exists and is active
-      const [rows] = await connection.execute('SELECT * FROM voucher WHERE code = ? AND valid = ?', [voucherCode, 'active']);
+      const search = await vouchers.find({code:voucherCode});
+      const rows = search.toArray();
       if (rows.length === 0) {
         // Voucher not found or not active
         return await interaction.followUp({ content: 'Invalid voucher code or the voucher has already been used/expired.', ephemeral: true });
@@ -36,12 +29,7 @@ module.exports = {
 
       // Mark the voucher as used
       const voucherId = rows[0].code;
-      await connection.execute('UPDATE voucher SET valid = ? WHERE code = ?', ['used', voucherId]);
-
-      // Close the MySQL connection
-      await connection.end();
-      console.log('MySQL connection closed.');
-
+      await vouchers.updateOne({code:voucherCode,valid:"used"});
       // Success message
       const successEmbed = new EmbedBuilder()
         .setTitle('Voucher Used Successfully')
