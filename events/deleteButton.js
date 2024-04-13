1d90a6caffe901c40e7dd4c2a245d0a8e104a6bd
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-04-13 16:19:09 +0530

    Update deleteButton.js

--- events/deleteButton.js
+++ events/deleteButton.js
@@ -5,11 +5,10 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'delete_ticket') return;
-    await interaction.deferReply();
     try {
       // Check if the user has admin permissions
       if (!interaction.member.permissions.has('PermissionsBitField.Flags.Admimistrator')) {
-        return await interaction.followUp({ content: 'You do not have permission to delete this ticket.', ephemeral: true });
+        return await interaction.reply({ content: 'You do not have permission to delete this ticket.', ephemeral: true });
       }
 
       // Send a confirmation message with a red button to confirm the ticket deletion
@@ -20,11 +19,11 @@ module.exports = {
 
       const confirmButton = { type:2,custom_id: 'confirm_delete', label: 'Confirm', style: 4 };
 
-      await interaction.followUp({ embeds: [confirmEmbed], components: [{ type: 1, components: [confirmButton] }] });
+      await interaction.reply({ embeds: [confirmEmbed], components: [{ type: 1, components: [confirmButton] }] });
 
     } catch (error) {
       console.error('Error handling delete button interaction:', error);
-      await interaction.followUp(error);
+      await interaction.reply(error);
     }
   },
 };
