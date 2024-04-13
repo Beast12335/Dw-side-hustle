3a1d131736d53af7587220a9456d886cd571396e
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-04-13 16:16:44 +0530

    Update closeButton.js

--- events/closeButton.js
+++ events/closeButton.js
@@ -5,11 +5,10 @@ module.exports = {
   async execute(interaction) {
     if (!interaction.isButton()) return;
     if (interaction.customId !== 'close_ticket') return;
-    await interaction.deferReply();
     try {
       // Check if the user has admin permissions
       if (!interaction.member.permissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
-        return await interaction.followUp({ content: 'You do not have permission to close this ticket.', ephemeral: true });
+        return await interaction.reply({ content: 'You do not have permission to close this ticket.', ephemeral: true });
       }
 
       // Send a confirmation message with a red button to confirm the ticket closing
@@ -20,11 +19,11 @@ module.exports = {
 
       const confirmButton = {type:2, custom_id: 'confirm_close', label: 'Confirm', style: 4 };
 
-      await interaction.followUp({ embeds: [confirmEmbed], components: [{ type: 1, components: [confirmButton] }] });
+      await interaction.reply({ embeds: [confirmEmbed], components: [{ type: 1, components: [confirmButton] }] });
 
     } catch (error) {
       console.error('Error handling close button interaction:', error);
-      await interaction.followUp(error);
+      await interaction.reply(error);
     }
   },
 };
