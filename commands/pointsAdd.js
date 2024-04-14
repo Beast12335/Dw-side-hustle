567e1ce43f9025b7b92ad4d5f714378caf5c7e42
Beast12335 <97028489+Beast12335@users.noreply.github.com>
2024-04-14 13:50:10 +0530

    Create pointsAdd.js

---
+++ commands/pointsAdd.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField,EmbedBuilder } = require('discord.js');
+const pointsModel = require('../db/points.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('points-add')
+    .setDescription('Add points to a user\'s balance.')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('Select a user')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('amount')
+        .setDescription('Amount of points to add')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await interaction.deferReply();
+
+    try {
+      // Check if the user invoking the command is an admin
+      if (!interaction.member.permissions.has(PermissionsBitField.Flags.ADMINISTRATOR)) {
+        const errorEmbed = new EmbedBuilder()
+          .setColor('#FF0000')
+          .setDescription('You do not have the necessary permissions to use this command.');
+
+        return interaction.followUp({ embeds: [errorEmbed] });
+      }
+
+      const selectedUser = interaction.options.getUser('user');
+      const amountToAdd = interaction.options.getInteger('amount');
+
+      // Check if the user has a points account
+      const userPoints = await pointsModel.findOne({ id: selectedUser.id });
+
+      if (!userPoints) {
+        const errorEmbed = new EmbedBuilder()
+          .setColor('#FF0000')
+          .setDescription('User does not have a points account.');
+
+        return interaction.followUp({ embeds: [errorEmbed] });
+      }
+
+      // Update the user's balance points
+      await pointsModel.updateOne({ id: selectedUser.id }, { $inc: { balance: amountToAdd } });
+
+      const successEmbed = new EmbedBuilder()
+        .setColor('#00FF00')
+        .setDescription(`${amountToAdd} points added to ${selectedUser.username}'s balance.`);
+
+      await interaction.followUp({ embeds: [successEmbed] });
+    } catch (error) {
+      console.error(error);
+      const errorEmbed = new EmbedBuilder()
+        .setColor('#FF0000')
+        .setDescription('Error processing the command.');
+
+      await interaction.followUp({ embeds: [errorEmbed] });
+    }
+  },
+};
